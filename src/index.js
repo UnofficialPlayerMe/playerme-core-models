3e68d1914029638364c7c831e285df69bc3c7c7e
Zazz <captain.zazz@gmail.com>
2016-04-11 00:19:19 +0100

    Uncommit dist files until the format they're exported in has been checked

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import UserExtendedModel from './models/UserExtendedModel';
 import CommentModel from './models/CommentModel';
 import ActivityModel from './models/ActivityModel';
 import GameModel from './models/GameModel';
+import GameExtendedModel from './models/GameExtendedModel';
 
-export {UserModel as UserModel};
-export {UserExtendedModel as UserExtendedModel};
-export {CommentModel as CommentModel};
-export {ActivityModel as ActivityModel};
-export {GameModel as GameModel};
+export {
+    UserModel,
+    UserExtendedModel,
+    CommentModel,
+    ActivityModel,
+    GameModel,
+    GameExtendedModel
+};
