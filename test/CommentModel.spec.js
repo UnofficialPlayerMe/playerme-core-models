5f4a46313eaf1bd6ff226899518b593810cbda07
Zazz <captain.zazz@gmail.com>
2016-04-10 14:17:55 +0100

    Make minor layout changes to Comment spec

--- test/CommentModel.spec.js
+++ test/CommentModel.spec.js
@@ -7,39 +7,40 @@ describe("CommentModel", function() {
         var model = new CommentModel(RawCommentModel);
 
         // Test standard comment model
-        assertProperty( model,       'id',               'number'        );
-        assertProperty( model,       'userId',           'number'        );
-        assertProperty( model,       'activityUserId',   'number'        );
-        assertProperty( model,       'activityId',       'number'        );
-        assertProperty( model,       'post',             'string'        );
-        assertProperty( model,       'postRaw',          'string'        );
-        assertProperty( model,       'metas',            'array'         );
-        assertProperty( model,       'createdAt',        'Date'          );
-        assertProperty( model,       'updatedAt',        'Date'          );
-        assertProperty( model,       'editedAt',         'null'          );
-        assertProperty( model,       'deletedAt',        'null'          );
-        assertProperty( model,       'user',             'UserModel'     );
-        assertProperty( model,       'userIsBlocked',    'boolean'       );
-        assertProperty( model,       'hasLiked',         'boolean'       );
-        assertProperty( model,       'url',              'string'        );
-        assertProperty( model,       'showDelete',       'boolean'       );
-        assertProperty( model,       'showEdit',         'boolean'       );
-        assertProperty( model,       'isOwnComment',     'boolean'       );
-        assertProperty( model,       'likesCount',       'number'        );
+        assertProperty( model, 'id',             'number'    );
+        assertProperty( model, 'userId',         'number'    );
+        assertProperty( model, 'activityUserId', 'number'    );
+        assertProperty( model, 'activityId',     'number'    );
+        assertProperty( model, 'post',           'string'    );
+        assertProperty( model, 'postRaw',        'string'    );
+        assertProperty( model, 'metas',          'array'     );
+        assertProperty( model, 'createdAt',      'Date'      );
+        assertProperty( model, 'updatedAt',      'Date'      );
+        assertProperty( model, 'editedAt',       'null'      );
+        assertProperty( model, 'deletedAt',      'null'      );
+        assertProperty( model, 'user',           'UserModel' );
+        assertProperty( model, 'userIsBlocked',  'boolean'   );
+        assertProperty( model, 'hasLiked',       'boolean'   );
+        assertProperty( model, 'url',            'string'    );
+        assertProperty( model, 'showDelete',     'boolean'   );
+        assertProperty( model, 'showEdit',       'boolean'   );
+        assertProperty( model, 'isOwnComment',   'boolean'   );
+        assertProperty( model, 'likesCount',     'number'    );
 
-        // Test metas
+        // Test meta
         assertProperty(model.metas, '0', 'CommentMeta');
         var meta = model.metas[0];
 
-        assertProperty( meta, 'images',       'array'    );
-        assertProperty( meta, 'url',          'string'   );
-        assertProperty( meta, 'title',        'string'   );
-        assertProperty( meta, 'description',  'string'   );
-        assertProperty( meta, 'provider',     'string'   );
-        assertProperty( meta, 'content',      'string'   );
-        assertProperty( meta, 'isInternal',   'boolean'  );
-        assertProperty( meta, 'thumbnail',    'string'   );
-        assertMethod(   meta, 'isPhoto', [],  'boolean', meta.content == 'photo');
-        assertMethod(   meta, 'isVideo', [],  'boolean', meta.content == 'video');
+        assertProperty( meta, 'images',      'array'   );
+        assertProperty( meta, 'url',         'string'  );
+        assertProperty( meta, 'title',       'string'  );
+        assertProperty( meta, 'description', 'string'  );
+        assertProperty( meta, 'provider',    'string'  );
+        assertProperty( meta, 'content',     'string'  );
+        assertProperty( meta, 'isInternal',  'boolean' );
+        assertProperty( meta, 'thumbnail',   'string'  );
+
+        assertMethod( meta, 'isPhoto', [], 'boolean', meta.content == 'photo' );
+        assertMethod( meta, 'isVideo', [], 'boolean', meta.content == 'video' );
     });
 });
