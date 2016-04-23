dde7cde6b38edd378ea7712dfdca6719b81958c5
Zazz <captain.zazz@gmail.com>
2016-04-23 21:12:48 +0100

    Add html karma test reporter

--- karma.conf.js
+++ karma.conf.js
@@ -23,6 +23,20 @@ module.exports = function(config) {
 
     webpackServer: {
       noInfo: true
+    },
+
+    reporters: [
+      'progress',
+      'html'
+    ],
+    htmlReporter: {
+      outputDir:       'test_results', // where to put the reports
+      templatePath:    null,           // set if you moved jasmine_template.html
+      focusOnFailures: true,           // reports show failures on start
+      namedFiles:      true,           // name files instead of creating sub-directories
+      pageTitle:       null,           // page title for reports; browser info by default
+      urlFriendlyName: false,          // simply replaces spaces with _ for files/dirs
+      reportName:      'karma'         // report summary filename; browser info by default
     }
 
   });
